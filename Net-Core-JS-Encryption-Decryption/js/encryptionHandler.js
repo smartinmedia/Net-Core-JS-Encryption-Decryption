c72cc00ca44d0ed6f906c8f4afca2326b08e6e66
martinweihrauch <info@smartinmedia.com>
2019-11-12 16:59:24 +0100

    Binary decryption fast with WebCrypto (5 MB in 12 ms)

--- Net-Core-JS-Encryption-Decryption/js/encryptionHandler.js
+++ Net-Core-JS-Encryption-Decryption/js/encryptionHandler.js
@@ -1,5 +1,38 @@
 ﻿function encryptionHandler() {
 
+
+
+    /*
+     PasswordDerivationOptions (options):
+     {
+        "DerivationType": "scrypt", // optionally: rfc
+        "Salt": "3a069e9126af66a839067f8a272081136d8ce63ed72176dc8a29973d2b15361f", //SALT must be in Hex
+        "Cost": 16384, //only for DerivationType "scrypt", not for "rfc"
+        "BlockSize": 8, //only for DerivationType "scrypt", not for "rfc"
+        "Parallel": 1, //only for DerivationType "scrypt", not for "rfc"
+        "KeySizeInBytes": 32,
+        "DerivationIterations": 0 // Only for DerivationType "rfc", not needed for "scrypt"
+
+    }
+    */
+
+    /*
+     * Simple text-based Encryption
+     */
+
+    this.encrypt = function (plainText, passPhrase, options) {
+        var that = this;
+        var rawPlainData;
+        options = autocompleteOptions(options);
+        var derivedKey = that.getDerivedKey(passPhrase, options);
+        rawPlainData = string2arraybuffer(plainText);
+        return webcryptoBinaryEncrypt(rawPlainData, derivedKey, options, true);
+    }
+
+    /*
+     * Simple text-based Decryption
+     */
+
     this.decrypt = function (encryptedData, passPhrase) {
         var that = this;
         var cO = JSON.parse(encryptedData);
@@ -8,8 +41,11 @@
         return webcryptoBinaryDecrypt(ciphertext, dKey, cO, true);
     }
 
-    
-    // Binary must be a UInt8Array!!
+    /*
+    * Binary Decryption: rawEncryptedData must be in UintArray, passPhrase regular text
+     * returnUtf8Text: if true, it returns text (assuming, text was encrypted and not binary data)
+     * returnUtf8Text: if false, it returns binary
+    */
 
     this.decryptBinary = function (rawEncryptedData, passPhrase, options, returnUtf8Text = false) {
         var that = this;
@@ -17,13 +53,30 @@
         return webcryptoBinaryDecrypt(rawEncryptedData, derivedKey, options, returnUtf8Text);
     }
 
+    /*
+    * Binary Decryption with derived Key: as decryptBinary, but with derived key in Uint8array.
+     * This has the purpose that if deriving the key takes a lot of time and you have multiple
+     * files to decrypt, you just derive the key once and decrypt file by file without losing time.
+    */
+
     this.decryptBinaryWithDerivedKey = function (rawEncryptedData, derivedKey, options, returnUtf8Text = false) {
         return webcryptoBinaryDecrypt(rawEncryptedData, derivedKey, options, returnUtf8Text);
     }
 
-    
-    
+    /*
+    * Binary Encryption with derived Key: Encrypt binary data
+    */
+
+    this.encryptBinary = function (rawPlainData, derivedKey, options, returnFullCryptoObject = false) {
+        options = autocompleteOptions(options);
+        return webcryptoBinaryEncrypt(rawPlainData, derivedKey, options, returnFullCryptoObject);
+    }
+
+
+    // To manually derive a crypto key, e. g. a Scrypt-key
     // Returns PBKDF2 key
+    //
+
     this.getDerivedKey = function(passPhrase, options)
     {
         var cO = options;
@@ -49,9 +102,9 @@
     function webcryptoBinaryDecrypt(rawEncryptedData, derivedKey, options, returnUtf8Text = true) {
         return new Promise(function (resolve, reject) {
             var iv = _base64ToArrayBuffer(options.AesRijndaelIv);
-            crypto.subtle.importKey("raw", derivedKey, "aes-cbc", false, ["decrypt"])
+            crypto.subtle.importKey("raw", derivedKey, "AES-CBC", false, ["encrypt", 'decrypt'])
                 .then(function (key) {
-                    return crypto.subtle.decrypt({ name: "aes-cbc", iv: iv }, key, rawEncryptedData);
+                    return crypto.subtle.decrypt({ 'name': 'AES-CBC', iv: iv }, key, rawEncryptedData);
                 },
                     reject)
                 .then(function (plainText) {
@@ -67,34 +120,16 @@
 
     }
 
-  
-    /*
-     * First, get your derived key, then you can decrypt binary data to binary with this
-     *
-     */
 
-    /*
-     PasswordDerivationOptions (options):
-     {
-            "DerivationType": "scrypt", // optionally: rfc
-            "Salt": "3a069e9126af66a839067f8a272081136d8ce63ed72176dc8a29973d2b15361f", //SALT must be in Hex
-            "Cost": 16384, //only for DerivationType "scrypt", not for "rfc"
-            "BlockSize": 8, //only for DerivationType "scrypt", not for "rfc"
-            "Parallel": 1, //only for DerivationType "scrypt", not for "rfc"
-            "KeySizeInBytes": 32,
-            "DerivationIterations": 0 // Only for DerivationType "rfc", not needed for "scrypt"
-
-        }
-        */
      
 
     // Send rawPlainData in Uint8Array!
     function webcryptoBinaryEncrypt(rawPlainData, derivedKey, options, returnFullCryptoObject = false) {
         return new Promise(function (resolve, reject) {
             var iv = _base64ToArrayBuffer(options.AesRijndaelIv);
-            crypto.subtle.importKey("raw", derivedKey, "aes-cbc", false, ["encrypt"])
+            crypto.subtle.importKey("raw", derivedKey, 'AES-CBC', false, ["encrypt", 'decrypt'])
                 .then(function (key) {
-                        return crypto.subtle.encrypt({ name: "aes-cbc", iv: iv }, key, rawPlainData);
+                    return crypto.subtle.encrypt({ 'name': 'AES-CBC', iv: iv }, key, rawPlainData);
                     },
                     reject)
                 .then(function (cipherText) {
@@ -143,243 +178,9 @@
         return options;
     }
 
-    this.encryptBinary = function (rawPlainData, derivedKey, options, returnFullCryptoObject = false) {
-        options = autocompleteOptions(options);
-        return webcryptoBinaryEncrypt(rawPlainData, derivedKey, options, returnFullCryptoObject);
-    }
 
 
-
-    this.encrypt = function (plainText, passPhrase, options) {
-        var that = this;
-        var rawPlainData;
-        options = autocompleteOptions(options);
-        var derivedKey = that.getDerivedKey(passPhrase, options);
-        rawPlainData = string2arraybuffer(plainText);
-        return webcryptoBinaryEncrypt(rawPlainData, derivedKey, options, true);
-    }
-
-    this.transformTextToHex = function(text) {
-        var utf8 = CryptoJS.enc.Utf8.parse(text);
-        return CryptoJS.enc.Hex.stringify(utf8);
-    }
-
-    function _base64ToArrayBuffer(base64) {
-        var binary_string = window.atob(base64);
-        var len = binary_string.length;
-        var bytes = new Uint8Array(len);
-        for (var i = 0; i < len; i++) {
-            bytes[i] = binary_string.charCodeAt(i);
-        }
-        return bytes.buffer;
-    }
-
-    function _arrayBufferToBase64(buffer) {
-        var binary = '';
-        var bytes = new Uint8Array(buffer);
-        var len = bytes.byteLength;
-        for (var i = 0; i < len; i++) {
-            binary += String.fromCharCode(bytes[i]);
-        }
-        return window.btoa(binary);
-    }
-
-    function failAndLog(error) {
-        console.log(error);
-    }
-
-    function hexStringToUint8Array(hexString) {
-        if (hexString.length % 2 != 0)
-            throw "Invalid hexString";
-        var arrayBuffer = new Uint8Array(hexString.length / 2);
-
-        for (var i = 0; i < hexString.length; i += 2) {
-            var byteValue = parseInt(hexString.substr(i, 2), 16);
-            if (byteValue == NaN)
-                throw "Invalid hexString";
-            arrayBuffer[i / 2] = byteValue;
-        }
-
-        return arrayBuffer;
-    }
-
-    function bytesToHexString(bytes) {
-        if (!bytes)
-            return null;
-
-        bytes = new Uint8Array(bytes);
-        var hexBytes = [];
-
-        for (var i = 0; i < bytes.length; ++i) {
-            var byteString = bytes[i].toString(16);
-            if (byteString.length < 2)
-                byteString = "0" + byteString;
-            hexBytes.push(byteString);
-        }
-
-        return hexBytes.join("");
-    }
-    function asciiToUint8Array(str) {
-        var chars = [];
-        for (var i = 0; i < str.length; ++i)
-            chars.push(str.charCodeAt(i));
-        return new Uint8Array(chars);
-    }
-    function bytesToASCIIString(bytes) {
-        return String.fromCharCode.apply(null, new Uint8Array(bytes));
-    }
-
-    function hexToArrayBuffer(hex) {
-        if (typeof hex !== 'string') {
-            throw new TypeError('Expected input to be a string');
-        }
-
-        if ((hex.length % 2) !== 0) {
-            throw new RangeError('Expected string to be an even number of characters');
-        }
-
-        var view = new Uint8Array(hex.length / 2);
-
-        for (var i = 0; i < hex.length; i += 2) {
-            view[i / 2] = parseInt(hex.substring(i, i + 2), 16);
-        }
-
-        return view.buffer;
-    }
-
-    function arrayBufferToHex(arrayBuffer) {
-        if (typeof arrayBuffer !== 'object' || arrayBuffer === null || typeof arrayBuffer.byteLength !== 'number') {
-            throw new TypeError('Expected input to be an ArrayBuffer');
-        }
-
-        var view = new Uint8Array(arrayBuffer)
-        var result = '';
-        var value;
-
-        for (var i = 0; i < view.length; i++) {
-            value = view[i].toString(16);
-            result += (value.length === 1 ? '0' + value : value);
-        }
-
-        return result;
-    }
-
-    // This is free and unencumbered software released into the public domain.
-
-    // Marshals a string to an Uint8Array.
-    function encodeUTF8(s) {
-        var i = 0, bytes = new Uint8Array(s.length * 4);
-        for (var ci = 0; ci != s.length; ci++) {
-            var c = s.charCodeAt(ci);
-            if (c < 128) {
-                bytes[i++] = c;
-                continue;
-            }
-            if (c < 2048) {
-                bytes[i++] = c >> 6 | 192;
-            } else {
-                if (c > 0xd7ff && c < 0xdc00) {
-                    if (++ci >= s.length)
-                        throw new Error('UTF-8 encode: incomplete surrogate pair');
-                    var c2 = s.charCodeAt(ci);
-                    if (c2 < 0xdc00 || c2 > 0xdfff)
-                        throw new Error('UTF-8 encode: second surrogate character 0x' + c2.toString(16) + ' at index ' + ci + ' out of range');
-                    c = 0x10000 + ((c & 0x03ff) << 10) + (c2 & 0x03ff);
-                    bytes[i++] = c >> 18 | 240;
-                    bytes[i++] = c >> 12 & 63 | 128;
-                } else bytes[i++] = c >> 12 | 224;
-                bytes[i++] = c >> 6 & 63 | 128;
-            }
-            bytes[i++] = c & 63 | 128;
-        }
-        return bytes.subarray(0, i);
-    }
-
-    // Unmarshals a string from an Uint8Array.
-    function decodeUTF8(bytes) {
-        var i = 0, s = '';
-        while (i < bytes.length) {
-            var c = bytes[i++];
-            if (c > 127) {
-                if (c > 191 && c < 224) {
-                    if (i >= bytes.length)
-                        throw new Error('UTF-8 decode: incomplete 2-byte sequence');
-                    c = (c & 31) << 6 | bytes[i++] & 63;
-                } else if (c > 223 && c < 240) {
-                    if (i + 1 >= bytes.length)
-                        throw new Error('UTF-8 decode: incomplete 3-byte sequence');
-                    c = (c & 15) << 12 | (bytes[i++] & 63) << 6 | bytes[i++] & 63;
-                } else if (c > 239 && c < 248) {
-                    if (i + 2 >= bytes.length)
-                        throw new Error('UTF-8 decode: incomplete 4-byte sequence');
-                    c = (c & 7) << 18 | (bytes[i++] & 63) << 12 | (bytes[i++] & 63) << 6 | bytes[i++] & 63;
-                } else throw new Error('UTF-8 decode: unknown multibyte start 0x' + c.toString(16) + ' at index ' + (i - 1));
-            }
-            if (c <= 0xffff) s += String.fromCharCode(c);
-            else if (c <= 0x10ffff) {
-                c -= 0x10000;
-                s += String.fromCharCode(c >> 10 | 0xd800)
-                s += String.fromCharCode(c & 0x3FF | 0xdc00)
-            } else throw new Error('UTF-8 decode: code point 0x' + c.toString(16) + ' exceeds UTF-16 reach');
-        }
-        return s;
-    }
-
-    function byteArrayToWordArray(ba) {
-        var wa = [],
-            i;
-        for (i = 0; i < ba.length; i++) {
-            wa[(i / 4) | 0] |= ba[i] << (24 - 8 * i);
-        }
-
-        return CryptoJS.lib.WordArray.create(wa, ba.length);
-    }
-
-    function wordToByteArray(word, length) {
-        var ba = [],
-            i,
-            xFF = 0xFF;
-        if (length > 0)
-            ba.push(word >>> 24);
-        if (length > 1)
-            ba.push((word >>> 16) & xFF);
-        if (length > 2)
-            ba.push((word >>> 8) & xFF);
-        if (length > 3)
-            ba.push(word & xFF);
-
-        return ba;
-    }
-
-    function wordArrayToByteArray(wordArray, length) {
-        if (wordArray.hasOwnProperty("sigBytes") && wordArray.hasOwnProperty("words")) {
-            length = wordArray.sigBytes;
-            wordArray = wordArray.words;
-        }
-
-        var result = [],
-            bytes,
-            i = 0;
-        while (length > 0) {
-            bytes = wordToByteArray(wordArray[i], Math.min(4, length));
-            length -= bytes.length;
-            result.push(bytes);
-            i++;
-        }
-        return [].concat.apply([], result);
-    }
-    function arraybuffer2string(buf) {
-        return String.fromCharCode.apply(null, new Uint16Array(buf));
-    }
-    function string2arraybuffer(str) {
-        var buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
-        var bufView = new Uint16Array(buf);
-        for (var i = 0, strLen = str.length; i < strLen; i++) {
-            bufView[i] = str.charCodeAt(i);
-        }
-        return buf;
-    }
-
+   
 }
 
 
